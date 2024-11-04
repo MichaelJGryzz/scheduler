a63afac2a40e2c567693fe2cd13994dce9e5ee9c
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-03 21:14:18 -0500

    Utilize Jest mocks for module isolation and async UI testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
-afterEach(cleanup);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { queryByText, findByText } = render(<Application />);
 
-it("renders without crashing", () => {
-  render(<Application />);
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
