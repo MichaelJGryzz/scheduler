7bd5ec5b1af3b06c880aa97779e9beb01e13f72c
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-04 17:37:03 -0500

    Complete integration test for appointment booking with debugging

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,6 +10,7 @@ import {
   getByAltText,
   getAllByTestId,
   prettyDOM,
+  queryByText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -28,7 +29,7 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     // 1. Render the Application
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
     // 2. Wait until the text "Archie Cohen" is displayed
     await findByText(container, "Archie Cohen");
     // 3. Click the "Add" button on the first empty appointment
@@ -44,8 +45,13 @@ describe("Application", () => {
     // 6. Click the "Save" button on that same appointment
     fireEvent.click(getByText(appointment, "Save"));
     // 7. Check that the element with the text "Saving" is displayed
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
     // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed
+    await findByText(appointment, "Lydia Miller-Jones");
     // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining"
-    console.log(prettyDOM(appointment));
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
